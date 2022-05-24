a56622e0bd186e74c27f5ef04d0171b12fdab82c
martinbogado <martinbogado@live.com.ar>
2022-05-24 16:57:19 -0300

    fixing styles in landing

--- components/Landing/Faqs/Faqs.tsx
+++ components/Landing/Faqs/Faqs.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react';
-import { Faq } from '../../../typings';
 import style from './Faqs.module.scss';
+
+interface Faq {
+  question: string,
+  answer: string
+}
 
 interface Prop {
     data: Faq[],
