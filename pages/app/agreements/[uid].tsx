3b7b2efe5207e8a377a283c16ddcfde914bef705
martinbogado <martinbogado@live.com.ar>
2022-05-25 10:53:22 -0300

    changes in Annuity Agreeement Details

--- pages/app/agreements/[uid].tsx
+++ pages/app/agreements/[uid].tsx
@@ -6,6 +6,8 @@ import { Contract } from "../../../hooks/Contract/useContract";
 import { useEffect, useState } from "react";
 import { useTokenValue } from "../../../hooks";
 
+import style from '../../../styles/components/agreement.module.scss';
+
 const Details = () => {
   const router = useRouter();
   const { uid } = router.query;
@@ -49,11 +51,23 @@ const Details = () => {
   }, [results]);
 
   return (
-    <div>
+    <div className={style.container}>
+      <p>Coso</p>
+
       <p>Borrower: {borrower}</p>
       <p>Lender: {lender}</p>
       <p>Deposit: ${depositValue}</p>
       <p>Collateral: ${collateralValue}</p>
+      <p>repaidAmt: {repaidAmt}</p>
+
+      <p>futureValue: {futureValue}</p>
+      <p>start: {start}</p>
+      <p>duration: {duration}</p>
+      <p>rate: {rate}</p>
+      <p>status: {status}</p>
+      <p>minReqCollateral: {minReqCollateral}</p>
+      <p>isLiquidationRequired: {isLiquidationRequired}</p>
+      
     </div>
   );
 };
