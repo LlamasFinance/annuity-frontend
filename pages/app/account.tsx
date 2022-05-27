92b8189e8f37c4284c519960b20e2ab845d2a950
Himanshu bhatt <himanshubb109@gmail.com>
2022-05-27 23:44:13 +0530

    added sorting/filter feature in market section

--- pages/app/account.tsx
+++ pages/app/account.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React , {useState,useEffect} from "react";
 import { Table } from "web3uikit";
 import { Moralis } from "moralis";
 import { useMoralis } from "react-moralis";
@@ -10,6 +10,8 @@ import { Contract } from "../../hooks/Contract/useContract";
 import { STATUS } from "../../constants";
 import { useTokenValue } from "../../hooks";
 import Link from "next/link";
+import { FaSort } from "react-icons/fa";
+import style from "./account.module.css";
 
 interface Props {}
 
@@ -17,6 +19,30 @@ const Account = (props: Props) => {
   const { results: agreements } = useFetchUserAgreements();
   const { account } = useMoralis();
   const { getValue } = useTokenValue({});
+  const [order,setOrder]=useState("ASC");
+  const [agreementsArray,setAgreementsArray]=useState< Moralis.Object<Contract.AgreementDetails>[]>([]);
+
+  const sorting=(basis:string):void=>{
+    if(order==="ASC"){
+     const result=agreements.sort((a:any,b:any)=>
+     a.attributes[basis].toLowerCase() >  b.attributes[basis].toLowerCase() ? 1:-1
+     );
+     setOrder("DSC");
+     setAgreementsArray(result);
+ }
+    if(order==="DSC"){
+     const result=agreements.sort((a:any,b:any)=>
+     a.attributes[basis].toLowerCase() <  b.attributes[basis].toLowerCase() ? 1:-1
+     );
+     setOrder("ASC");
+     setAgreementsArray(result);
+ }
+  }
+  console.log(agreementsArray);
+
+  useEffect(()=>{
+    setAgreementsArray(agreements);
+  },[agreements]);
   return (
     <div className="Account">
       <div className="Account__header">
@@ -31,13 +57,13 @@ const Account = (props: Props) => {
         columnsConfig="1fr 1fr 1fr 1fr 1fr 1fr"
         header={[
           <span>Position</span>,
-          <span>ID</span>,
-          <span>Status</span>,
-          <span>USD Deposit ($)</span>,
-          <span>ETH Collateral</span>,
+          <span onClick={()=>sorting("uid")}>ID<FaSort className={style.icon} /></span>,
+          <span onClick={()=>sorting("status")}>Status<FaSort className={style.icon} /></span>,
+          <span onClick={()=>sorting("deposit")}> Deposit ($) <FaSort className={style.icon} /></span>,
+          <span onClick={()=>sorting("collateral")}> Collateral <FaSort className={style.icon} /></span>,
           <span>Details</span>,
         ]}
-        data={agreements.map(({ createdAt, attributes }) => {
+        data={agreementsArray.map(({ createdAt, attributes }) => {
           const { lender, uid, status, deposit, collateral } = attributes;
           let { inEth } = getValue({
             amount: collateral,
--- pages/app/market.tsx
+++ pages/app/market.tsx
@@ -1,5 +1,5 @@
 import { GetServerSideProps } from "next";
-import React from "react";
+import React ,{useState,useEffect} from "react";
 import Moralis from "moralis";
 import { Contract } from "../../hooks/Contract/useContract";
 import ProposeButton from "../../components/Contract/ProposeButton";
@@ -8,13 +8,63 @@ import { Table } from "web3uikit";
 import { APP_ID, SERVER_URL, STATUS } from "../../constants";
 import useFetchAllAgreements from "../../hooks/App/db/useFetchAllAgreements";
 import Link from "next/link";
+import { useMoralis } from "react-moralis";
+import { useTokenValue } from "../../hooks";
+import { FaSort } from "react-icons/fa";
+import style from "./account.module.css";
 
 interface Props {
-  //   agreements: Moralis.Object<Contract.AgreementDetails>[];
+    // agreements: Moralis.Object<Contract.AgreementDetails>[];
 }
 
+interface IAgreement{
+  borrower:string,
+  collateral:string,
+  createdAt:string,
+  deposit:string,
+  duration:string,
+  futureValue:string,
+  isLiquidationRequired:string,
+  minReqCollateral:string,
+  objectId:string,
+  rate:string,
+  repaidAmt:string,
+  start:string,
+  status:string,
+  uid:string,
+  updatedAt:string,
+}
 const Market = (props: Props) => {
   const { results: agreements } = useFetchAllAgreements();
+  const { account } = useMoralis();
+  const { getValue } = useTokenValue({});
+  const [order,setOrder]=useState("ASC");
+  const [agreementsArray,setAgreementsArray]=useState<IAgreement[]>([]);
+
+  const res=JSON.parse(JSON.stringify(agreements));
+
+  const sorting=(basis:string):void=>{
+    if(order==="ASC"){
+     const result=agreementsArray.sort((a:any,b:any)=>
+     a[basis].toLowerCase() >  b[basis].toLowerCase() ? 1:-1
+     );
+     setOrder("DSC");
+     setAgreementsArray(result);
+ }
+   else if(order==="DSC"){
+     const result=agreementsArray.sort((a:any,b:any)=>
+     a[basis].toLowerCase() <  b[basis].toLowerCase() ? 1:-1
+     );
+     setOrder("ASC");
+     setAgreementsArray(result);
+ }
+  }
+  
+useEffect(()=>{
+setAgreementsArray(res)
+},[agreements])
+
+  
   return (
     <div className="Account">
       <div className="Account__header">
@@ -27,23 +77,25 @@ const Market = (props: Props) => {
       <Table
         columnsConfig="1fr 1fr 1fr 1fr 1fr 1fr 1fr"
         header={[
-          <span>Status</span>,
-          <span>Amount ($USDC)</span>,
-          <span>APY (%)</span>,
-          <span>Duration (years)</span>,
-          <span>Date</span>,
-          <span>ID</span>,
-          <span>Details</span>,
+          <span ><label>Status</label><FaSort onClick={() => sorting("status")} className={style.icon} /> </span>,
+          <span ><label>Amount($)</label><FaSort onClick={() => sorting("deposit")} className={style.icon} /></span>,
+          <span ><label>APY(%)</label><FaSort onClick={() => sorting("rate")} className={style.icon} /></span>,
+          <span ><label>Duration(yrs)</label><FaSort onClick={() => sorting("duration")} className={style.icon} /></span>,
+          <span><label>Date<FaSort  onClick={() => sorting("createdAt")} className={style.icon} /></label></span>,
+          <span><label>ID</label><FaSort  onClick={() => sorting("uid")} className={style.icon} /></span>,
+          <span><label>Details</label></span>,
         ]}
-        data={agreements.map(({ createdAt, attributes }) => {
-          const { status, deposit, rate, duration, uid } = attributes;
+        
+        data={agreementsArray.map((agreement) => {
+          const { status, deposit, rate, duration, uid ,start,createdAt} = agreement;
+          
           return [
-            <span>{STATUS[Number(status)]}</span>,
-            <span>{Number(deposit) / 10 ** 6}</span>,
-            <span>{Number(rate) / 10}</span>,
-            <span>{duration}</span>,
-            <span>{createdAt?.toLocaleDateString()}</span>,
-            <span>{uid}</span>,
+            <span>{STATUS[Number(status)]}  </span>,
+            <span>{Number(deposit) / 10 ** 6} </span>,
+            <span>{Number(rate) / 10} </span>,
+            <span>{duration} </span>,
+            <span>{new Date(createdAt)?.toLocaleDateString()}</span>,
+            <span>{uid} </span>,
             <span>
               <Link href={`/app/agreements/${uid}`}>
                 <button className="btn-default btn">Details</button>
